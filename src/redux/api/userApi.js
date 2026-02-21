f7e469f0d73e7babb246d532eed76d0469b274d0
Vinil Dongri <dongrivinil@gmail.com>
2026-02-22 00:40:11 +0530

    fixed unUsed vars

--- src/redux/api/userApi.js
+++ src/redux/api/userApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { setIsAuthenticated, setUser } from "../features/userSlice";
-import ResetPassword from "../../components/user/ResetPassword";
+// import { setIsAuthenticated, setUser } from "../features/userSlice";
+// import ResetPassword from "../../components/user/ResetPassword";
 
 export const userApi = createApi({
     reducerPath: "userApi",
